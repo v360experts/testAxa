1f3580010da205ad3e32eb32e1df48e74fa08e24
“Williams” <williams.fontcha@gmail.com git config --global user.email williams.fontecha@gmail.com>
2021-09-17 15:55:07 -0500

    change 5

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+const { SecretClient } = require('@azure/keyvault-secrets');
+const { DefaultAzureCredential } = require('@azure/identity');
+const KEY_VAULT_URL = null || process.env['KEY_VAULT_URL'];
+const SECRET_NAME = null || process.env['SECRET_NAME'];
+
+
+
+function getKeyVaultCredentials() {
+  return new DefaultAzureCredential();
+}
+
+function getKeyVaultSecret(credentials) {
+  let keyVaultClient = new SecretClient(KEY_VAULT_URL, credentials);
+  return keyVaultClient.getSecret(SECRET_NAME);
+}
+
+app.get('/secret', function (req, res) {
+  let credentials = getKeyVaultCredentials();
+  getKeyVaultSecret(credentials)
+    .then(function (secret) {
+      res.send(`Your secret value is: ${secret.value}.`);
+    }).catch(function (err) {
+      res.send(err);
+    });
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  let credentials = getKeyVaultCredentials();
+  getKeyVaultSecret(credentials)
+    .then(function (secret) {
+      //res.send(`Your secret value is: ${secret.value}.`);
+      res.render('index', { title: `Express ${secret.value}.` });
+    }).catch(function (err) {
+      res.send(err);
+    });
+ 
 });
 
 module.exports = router;
