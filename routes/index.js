bd86f2635e69a151c21a7fb077577f7c41387910
“Williams” <williams.fontcha@gmail.com git config --global user.email williams.fontecha@gmail.com>
2021-10-05 18:06:21 -0500

    change 11

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 const { SecretClient } = require('@azure/keyvault-secrets');
 const { DefaultAzureCredential } = require('@azure/identity');
-const KEY_VAULT_URL = `https://keyvaultaxa.vault.azure.net`;// || process.env['KEY_VAULT_URL'];
+const { KeyClient } = require("@azure/keyvault-keys");
+ 
+// when deployed to an azure host the default azure credential will authenticate the specified user assigned managed identity
+//var credential = new DefaultAzureCredential({ managedIdentityClientId: userAssignedClientId });
+ 
+
+
+
+const KEY_VAULT_URL = `https://keyvaultaxa.vault.azure.net/`;// || process.env['KEY_VAULT_URL'];
 const SECRET_NAME = "PassAxa";// || process.env['SECRET_NAME'];
+
+
 
 
 
@@ -37,15 +47,11 @@ router.get('/secret', function (req, res) {
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  let credentials = getKeyVaultCredentials();
-  getKeyVaultSecret(credentials)
-    .then(function (secret) {
-      //res.send(`Your secret value is: ${secret.value}.`);
-      res.render('index', { title: `Express ${secret.value}.` });
-    }).catch(function (err) {
-      res.send(err);
-    });
- 
+  let credential = getKeyVaultCredentials();
+  const client = new KeyClient(KEY_VAULT_URL, credential);
+  const getResult = await client.getKey("PassAxa");
+  res.render('index', { title: `Express ${getResult}.` });
+
 });
 
 module.exports = router;
