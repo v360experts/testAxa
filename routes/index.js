ff7c147b28ecbb2353c8c6cc1ebb23310d4e7509
“Williams” <williams.fontcha@gmail.com git config --global user.email williams.fontecha@gmail.com>
2021-10-15 01:30:19 -0500

    volviendo a el change 7

--- routes/index.js
+++ routes/index.js
@@ -2,36 +2,16 @@ var express = require('express');
 var router = express.Router();
 const { SecretClient } = require('@azure/keyvault-secrets');
 const { DefaultAzureCredential } = require('@azure/identity');
-//import { setLogLevel } from "@azure/logger";
-const { KeyClient } = require("@azure/keyvault-keys");
-//setLogLevel("info");
-
-// when deployed to an azure host the default azure credential will authenticate the specified user assigned managed identity
-//var credential = new DefaultAzureCredential({ managedIdentityClientId: userAssignedClientId });
- 
-
-
-
-const KEY_VAULT_URL = `https://keyvaultaxa.vault.azure.net/`;// || process.env['KEY_VAULT_URL'];
-const SECRET_NAME = "PassAxa";// || process.env['SECRET_NAME'];
-
-
+const KEY_VAULT_URL = 'https://keyvaultaxa.vault.azure.net/' || process.env['KEY_VAULT_URL'];
+const SECRET_NAME = 'PassAxa' || process.env['SECRET_NAME'];
 
 
 
 function getKeyVaultCredentials() {
-  return new DefaultAzureCredential({
-    managedIdentityClientId: "d7f513cb-ee4d-4ecf-a893-0ec0f81ea33f"
-  });
-
-
-  /*const credential = new ManagedIdentityCredential("d7f513cb-ee4d-4ecf-a893-0ec0f81ea33f");
-  return credential;*/
+  return new DefaultAzureCredential();
 }
 
 function getKeyVaultSecret(credentials) {
-  console.log('KEY_VAULT_URL->',KEY_VAULT_URL);
-  console.log('SECRET_NAME->',SECRET_NAME);
   let keyVaultClient = new SecretClient(KEY_VAULT_URL, credentials);
   return keyVaultClient.getSecret(SECRET_NAME);
 }
@@ -49,11 +29,15 @@ router.get('/secret', function (req, res) {
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  /*let credential = getKeyVaultCredentials();
-  const client = new KeyClient(KEY_VAULT_URL, credential);*/
-  const getResult = "hhh";//await client.getKey("PassAxa");
-  res.render('index', { title: `Express ${getResult}.` });
-
+  let credentials = getKeyVaultCredentials();
+  getKeyVaultSecret(credentials)
+    .then(function (secret) {
+      //res.send(`Your secret value is: ${secret.value}.`);
+      res.render('index', { title: `Express ${secret.value}.` });
+    }).catch(function (err) {
+      res.send(err);
+    });
+ 
 });
 
 module.exports = router;
